4c9210f769761d00fd34abd560c13236354a0a09
Aldrich Medina <aldrichmbg@gmail.com>
2026-08-17 21:26:24 -0600

    While en python

--- Archivo4.py
+++ Archivo4.py
@@ -3,3 +3,14 @@ print("Hola Mundo")
 #for
 for numero in range(1, 6):
     print(f"Esta es la repetición número: {numero}")
+
+# Simulando un do-while en Python
+contador = 1
+
+while True:
+    print("El número es:", contador)
+    contador += 1
+    
+    # Condición para salir del ciclo
+    if contador > 3:
+        break
